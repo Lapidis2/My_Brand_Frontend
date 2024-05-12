836c99eec2dc74fa1971becadb35708a99ff5f30
HITAYEZU Jean Pierre <hitlapidis2@gmail.com>
2024-05-12 22:10:09 +0200

    new commit

--- comments.js
+++ comments.js
@@ -1,11 +1,14 @@
+// @ts-nocheck
+
 function addComment(){
-    //@ts-ignore
+    
     const userid=JSON.parse(localStorage.getItem("userid"))
-    //@ts-ignore
+
     const text= document.getElementById("commentText").value.trim()
     const blogId= window.location.href.split("=")[1]
     fetch(`https://my-brand-backend-tsc3.onrender.com/comments/createComment/${blogId}/user/${userid}`, {
         headers:{
+            method: "POST",
             "Content-Type": "application/json"
         },
         method: "POST",
@@ -25,12 +28,13 @@ function addComment(){
 const commentBtn= document.getElementById("comment")
 const likeBtn= document.getElementById("like")
 likeBtn?.addEventListener("click", async()=>{
-     //@ts-ignore
+     
     const userid=JSON.parse(localStorage.getItem("userid"))
-    //@ts-ignore
+    
     const blogId= window.location.href.split("=")[1]
     fetch(`https://my-brand-backend-tsc3.onrender.com/like/${blogId}/user/${userid}`, {
         headers:{
+            method: "POST",
             "Content-Type": "application/json"
         },
         method: "POST",
