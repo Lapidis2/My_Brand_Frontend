f45654739d32de60a6bec476edf70e7687f695c7
HITAYEZU Jean Pierre <hitlapidis2@gmail.com>
2024-05-16 02:10:30 +0200

    new commit

--- comments.js
+++ comments.js
@@ -2,11 +2,11 @@
 
 function addComment(){
     
-    const userid=JSON.parse(localStorage.getItem("userid"))
+    const userId=JSON.parse(localStorage.getItem("userId"))
 
     const text= document.getElementById("commentText").value.trim()
     const blogId= window.location.href.split("=")[1]
-    fetch(`https://my-brand-backend-tsc3.onrender.com/comments/createComment/${blogId}/user/${userid}`, {
+    fetch(`https://my-brand-backend-tsc3.onrender.com/comments/createComment/${blogId}/user/${userId}`, {
         headers:{
             method: "POST",
             "Content-Type": "application/json"
@@ -29,15 +29,16 @@ const commentBtn= document.getElementById("comment")
 const likeBtn= document.getElementById("like")
 likeBtn?.addEventListener("click", async()=>{
      
-    const userid=JSON.parse(localStorage.getItem("userid"))
+    const userId=JSON.parse(localStorage.getItem("userId"))
     
     const blogId= window.location.href.split("=")[1]
-    fetch(`https://my-brand-backend-tsc3.onrender.com/like/${blogId}/user/${userid}`, {
-        headers:{
-            method: "POST",
+    fetch(`https://my-brand-backend-tsc3.onrender.com/like/${blogId}/user/${userId}`, {
+        method: "POST",
+    headers:{
+            
             "Content-Type": "application/json"
         },
-        method: "POST",
+        
     }).then(response=>{
         if(!response.ok){
             throw Error("Netwotk Error")
